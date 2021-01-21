272045938794f7945313f56041838ed03b6039de
TheKid <thekid@iMac-de-TheKid.local>
2021-01-20 22:02:44 -0300

    init project

---
+++ src/components/bigBanner/BigBanner.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import './BigBanner.scss';
+
+const BigBanner  = () => {
+    return (
+        <div className="bigBanner">
+            <h1>The Kid Big Banner</h1>
+            <h1>The Kid Big Banner</h1>
+            <h1>The Kid Big Banner</h1>
+        </div>
+        
+    )
+}
+
+
+export default BigBanner;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BigBanner from './components/bigBanner/BigBanner';
+
+const app =  document.querySelector('#app');
+ReactDOM.render( <BigBanner />, app );
+
+
